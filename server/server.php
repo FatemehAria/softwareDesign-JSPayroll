4b18b27accefe93a5a3d8a6ae98dd962b345da73
FatemehAria <ftemewh@gmail.com>
2023-06-25 20:15:46 +0330

    till search

---
+++ server/server.php
@@ -0,0 +1,21 @@
+<?php
+// echo $_POST['firstname'];
+// echo $_POST['lastname'];
+// echo $_POST['nationalid'];
+// echo $_POST['contacrt'];
+// echo $_POST['personalid'];
+$str = "";
+foreach($_POST as $key => $value){
+    if(is_array($value)){
+        $str.="<pre>";
+        print_r("$key:$value[0]</br>");
+        $str.="</pre> ";
+    }else{
+        $str.="<pre>";
+        $str.="$key:$value</br>" ;
+        $str.="</pre> ";
+    }
+}
+echo $str;
+file_put_contents("../file_926.xml",$str,FILE_APPEND);
+header("Location:../show.html");
