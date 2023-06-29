1b03236dc6994eaa7a152c70cb625bc1f4a686e0
FatemehAria <ftemewh@gmail.com>
2023-06-29 20:38:15 +0330

    search fixed

--- server/server.php
+++ server/server.php
@@ -5,17 +5,17 @@
 // echo $_POST['contacrt'];
 // echo $_POST['personalid'];
 $str = "";
-foreach($_POST as $key => $value){
-    if(is_array($value)){
-        $str.="<pre>";
+foreach ($_POST as $key => $value) {
+    if (is_array($value)) {
+        $str .= "<pre>";
         print_r("$key:$value[0]</br>");
-        $str.="</pre> ";
-    }else{
-        $str.="<pre>";
-        $str.="$key:$value</br>" ;
-        $str.="</pre> ";
+        $str .= "</pre> ";
+    } else {
+        $str .= "<pre>";
+        $str .= "$key:$value</br>";
+        $str .= "</pre> ";
     }
 }
 echo $str;
-file_put_contents("../file_926.xml",$str,FILE_APPEND);
+file_put_contents("../file_926.xml", $str, FILE_APPEND);
 header("Location:../show.html");
