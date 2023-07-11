113480c8e3448698c591b6e813781ba0515e68a9
FatemehAria <ftemewh@gmail.com>
2023-07-11 16:04:43 +0330

    note

--- server/server.php
+++ server/server.php
@@ -1,16 +1,24 @@
 <?php
-$str = "";
+// $str = "";
+$str .= "<div>";
 foreach ($_POST as $key => $value) {
     if (is_array($value)) {
         $str .= "<pre>";
-        print_r("$key:$value[0]</br>");
-        $str .= "</pre> ";
+        $str .= "<p>";
+        print_r("$key:$value[0]");
+        $str .= "</p>";
+        $str .= "<button>Delete</button>";
+        $str .= "</pre>";
     } else {
         $str .= "<pre>";
-        $str .= "$key:$value</br>";
-        $str .= "</pre> ";
+        $str .= "<p>";
+        $str .= "$key:$value";
+        $str .= "</p>";
+        $str .= "<button>Delete</button>";
+        $str .= "</pre>";
     }
 }
-// echo $str .= "<p>-------------------------------------</p>";
+$str .= "<p>-------------------------------------</p>";
+$str .= "</div>";
 file_put_contents("../xmlFile.xml", $str, FILE_APPEND);
 header("Location:../show.html");
